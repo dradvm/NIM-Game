fb96c584e4a3106e4a57f8a12cf5b6c164675844
dradvm <duynnvn2511@gmail.com>
2025-03-24 13:55:48 +0700

    change text and test publish v3

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -26,7 +26,7 @@ export default memo(function Menu() {
             <div className="">
                 <MenuButton
                     image={shape}
-                    title={"Chơi trực tuyến ABCD"}
+                    title={"Chơi trực tuyến ABCDE"}
                     subTitle={"Chơi với mọi người trên thế giới"}
                     onClick={handleSelectMenuPvP}
                 />
